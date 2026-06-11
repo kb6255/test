e60d101d753153d975397981b78ebdc9bcb1caa4
kb6255 <625584525@qq.com>
2026-06-11 10:37:54 +0800

    bujianrong 'systemctl'

--- key_start_stop.py
+++ key_start_stop.py
@@ -1,51 +1,6 @@
-'''
-import RPi.GPIO as GPIO
-import subprocess
-import os
-import time
+# 该脚本自动启动与systemd服务不兼容。
 
-# 配置
-KEY_POWER = 26    #开关机按键
-KEY_MODE  = 19    #画面切换按键
-APP_PATH = "/home/kongbin/test/danshuangqiehuan.py"
-PY_CMD = "python3"
 
-GPIO.setmode(GPIO.BCM)
-#上拉输入
-GPIO.setup(KEY_POWER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(KEY_MODE,  GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-def is_app_running():
-    res = subprocess.run(["pgrep","-f",APP_PATH],capture_output=True,text=True)
-    return res.returncode == 0
-
-def send_switch_mode():
-    # 新建标记文件，主程序检测到自动切换
-    open("/tmp/cam_switch.flag","w").close()
-
-while True:
-    # ===== 按键1：开关机 =====
-    if GPIO.input(KEY_POWER) == 0:
-        time.sleep(0.02)
-        if GPIO.input(KEY_POWER) == 0:
-            while GPIO.input(KEY_POWER)==0:
-                time.sleep(0.01)
-            if is_app_running():
-                subprocess.run(["pkill","-f",APP_PATH])
-            else:
-                subprocess.Popen([PY_CMD,APP_PATH],cwd=os.path.dirname(APP_PATH))
-
-    # ===== 按键2：画面模式切换 =====
-    if GPIO.input(KEY_MODE) == 0:
-        time.sleep(0.02)
-        if GPIO.input(KEY_MODE) == 0:
-            while GPIO.input(KEY_MODE)==0:
-                time.sleep(0.01)
-            if is_app_running():
-                send_switch_mode()
-
-    time.sleep(0.05)
-    '''
 # 导入树莓派GPIO控制库，用于读取按键电平
 import RPi.GPIO as GPIO
 # 导入子进程模块，用来启停、查询、杀死主程序
